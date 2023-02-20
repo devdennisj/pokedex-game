23fada7cb189d5d37fcd78c589592b94a0cc117e
devdennisj <dev@dennisj.se>
2023-02-20 20:42:48 +0100

    Changes Pokedex to be lazy loaded because it uses dependencies that are not used in the rest of the application

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { lazy } from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 import RootLayout from './components/RootLayout';
 import { fetchGeneration } from './data/generation';
-import Pokedex from './pages/Pokedex';
 import World from './pages/World';
+
+const Pokedex = lazy(() => import('./pages/Pokedex'));
 
 import './index.css';
 
