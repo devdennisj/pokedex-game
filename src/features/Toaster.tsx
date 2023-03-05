6a2684fe71d2d7503316ebdbcaf5e43b497d4f55
devdennisj <dev@dennisj.se>
2023-03-05 16:32:35 +0100

    Limits number of toasts

--- src/features/Toaster.tsx
+++ src/features/Toaster.tsx
@@ -1,6 +1,21 @@
-import { Toaster as ProtoToaster } from 'react-hot-toast';
+import { useEffect } from 'react';
+import toast, {
+  Toaster as ProtoToaster,
+  useToasterStore,
+} from 'react-hot-toast';
+
+const TOAST_LIMIT = 5;
 
 function Toaster() {
+  const { toasts } = useToasterStore();
+
+  useEffect(() => {
+    toasts
+      .filter((t) => t.visible)
+      .filter((_, i) => i >= TOAST_LIMIT)
+      .forEach((t) => toast.dismiss(t.id));
+  }, [toasts]);
+
   return (
     <ProtoToaster
       position='top-right'
